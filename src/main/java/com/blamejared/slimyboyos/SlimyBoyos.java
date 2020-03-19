27ea1ccf50f716b390e7bc0dd87a6ec4b43af396
Jared <jaredlll08@gmail.com>
2020-03-20 01:49:01 +0200

    Fixed the item not rendering on the server

--- src/main/java/com/blamejared/slimyboyos/SlimyBoyos.java
+++ src/main/java/com/blamejared/slimyboyos/SlimyBoyos.java
@@ -1,32 +1,28 @@
 package com.blamejared.slimyboyos;
 
-import com.blamejared.slimyboyos.client.render.SlimeItemLayer;
 import com.blamejared.slimyboyos.events.*;
 import com.blamejared.slimyboyos.network.PacketHandler;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.*;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 
 
 @Mod("slimyboyos")
 public class SlimyBoyos {
     
     public SlimyBoyos() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-        PacketHandler.init();
         MinecraftForge.EVENT_BUS.register(new CommonEventHandler());
-        
+    }
+    private void setup(final FMLCommonSetupEvent event) {
+        PacketHandler.init();
     }
     
     private void doClientStuff(final FMLClientSetupEvent event) {
         MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
     }
-    
     
     
 }
