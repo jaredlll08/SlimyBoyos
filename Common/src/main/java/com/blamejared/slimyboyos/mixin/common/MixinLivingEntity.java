a3e36b1f13d3f45583055e6e82000134e7bc11f7
williambl <william@williambl.com>
2022-10-02 17:14:13 +0100

    Add slimyboyos:slime_absorb_blacklist tag

--- Common/src/main/java/com/blamejared/slimyboyos/mixin/common/MixinLivingEntity.java
+++ Common/src/main/java/com/blamejared/slimyboyos/mixin/common/MixinLivingEntity.java
@@ -1,16 +1,22 @@
 package com.blamejared.slimyboyos.mixin.common;
 
+import com.blamejared.slimyboyos.Constants;
 import com.blamejared.slimyboyos.api.IAbsorber;
 import com.blamejared.slimyboyos.platform.Services;
+import com.google.common.base.Suppliers;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
@@ -22,11 +28,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity implements IAbsorber {
     
     private static final EntityDataAccessor<ItemStack> DATA_ABSORBED = SynchedEntityData.defineId(LivingEntity.class, EntityDataSerializers.ITEM_STACK);
+    private static final Supplier<TagKey<Item>> SLIME_ABSORB_BLACKLIST = Suppliers.memoize(() -> TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(Constants.MOD_ID, "slime_absorb_blacklist")));
     
     @Unique
     public boolean slimyboyos$canAbsorb;
@@ -55,14 +63,17 @@ public abstract class MixinLivingEntity extends Entity implements IAbsorber {
             AABB boundingBox = this.getBoundingBox();
             List<ItemEntity> entities = this.level.getEntities(EntityType.ITEM, boundingBox, item -> item.isAlive() && !item.isPickable() && !item.getItem()
                     .isEmpty());
-            entities.stream().findFirst().ifPresent(item -> {
-                ItemStack stack = item.getItem();
-                ItemStack absorbedStack = stack.split(1);
-                slimyboyos$setAbsorbedItem(absorbedStack);
-                if(stack.isEmpty()) {
-                    item.discard();
-                }
-            });
+            entities.stream()
+                    .filter(item -> !item.getItem().is(SLIME_ABSORB_BLACKLIST.get()))
+                    .findFirst()
+                    .ifPresent(item -> {
+                        ItemStack stack = item.getItem();
+                        ItemStack absorbedStack = stack.split(1);
+                        slimyboyos$setAbsorbedItem(absorbedStack);
+                        if(stack.isEmpty()) {
+                            item.discard();
+                        }
+                    });
         }
     }
     
