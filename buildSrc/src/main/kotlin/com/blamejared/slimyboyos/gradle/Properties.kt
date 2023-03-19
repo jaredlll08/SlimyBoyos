c7ce2b684c3230cffa474d89a074472dee0ff043
Jared <jaredlll08@gmail.com>
2023-03-19 14:26:49 +0200

    Update first commit

--- buildSrc/src/main/kotlin/com/blamejared/slimyboyos/gradle/Properties.kt
+++ buildSrc/src/main/kotlin/com/blamejared/slimyboyos/gradle/Properties.kt
@@ -8,6 +8,6 @@ object Properties {
     const val AVATAR = "https://media.forgecdn.net/avatars/130/894/636463564739010146.png"
     const val CURSE_PROJECT_ID = "281993"
     const val CURSE_HOMEPAGE = "https://www.curseforge.com/minecraft/mc-mods/slimyboyos"
-    const val FIRST_COMMIT = "177ab9a3e75ad30d64e374e8b5f597ff844702b8"
+    const val FIRST_COMMIT = "4d67d202703dec63becb087ee915db4472a02ed0"
     const val GIT_REPO = "https://github.com/jaredlll08/SlimyBoyos"
 }
