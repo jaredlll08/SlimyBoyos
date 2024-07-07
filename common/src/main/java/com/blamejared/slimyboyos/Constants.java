d52d7a5095032315c321adf789a71f63379750f8
Jared <jaredlll08@gmail.com>
2024-07-07 09:14:55 -0600

    Fix tag folder. close #15

--- common/src/main/java/com/blamejared/slimyboyos/Constants.java
+++ common/src/main/java/com/blamejared/slimyboyos/Constants.java
@@ -2,16 +2,9 @@ package com.blamejared.slimyboyos;
 
 import com.google.common.base.Suppliers;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Pose;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,7 +16,7 @@ public class Constants {
     public static final String MOD_NAME = "SlimyBoyos";
     public static final Logger LOG = LogManager.getLogger(MOD_NAME);
     
-     public static final Supplier<TagKey<Item>> SLIMES_CANNOT_ABSORB = Suppliers.memoize(() -> TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "slimes_cannot_absorb")));
+    public static final Supplier<TagKey<Item>> SLIMES_CANNOT_ABSORB = Suppliers.memoize(() -> TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "slimes_cannot_absorb")));
     
     
 }
